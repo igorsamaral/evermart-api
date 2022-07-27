5a1c2a10b5d77de1d75200c636927ab389fae024
Igor Santana <igr.amaral@gmail.com>
2022-07-27 20:01:58 -0300

    feat: add método para criar novo customer

--- src/Presenters/CustomerPresenter.ts
+++ src/Presenters/CustomerPresenter.ts
@@ -1,14 +1,27 @@
 import { Request, Response } from 'express'
-
+import { CustomerService } from '../Services/customer/CustomerService'
 import { XlsGeneratorService } from '../Services/export/XlsGeneratorService'
 
 class CustomerPresenter {
+    private customerService: CustomerService
+
+    constructor() {
+        this.customerService = new CustomerService()
+    }
+
     async index(request: Request, response: Response) {
         return response.json({})
     }
 
     async store(request: Request, response: Response) {
-        return response.json({})
+        const data = request.body
+
+        try {
+            const customer = await this.customerService.create(data)
+            return response.json(customer)
+        } catch (error) {
+            console.error(error)
+        }
     }
 
     async generateXls(request: Request, response: Response) {
