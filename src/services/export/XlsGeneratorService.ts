3b22d467f84821236c960577fe41c4ff38764b8c
Igor Santana <igr.amaral@gmail.com>
2022-07-27 19:04:21 -0300

    feat: cria service para gerar XLS

---
+++ src/services/export/XlsGeneratorService.ts
@@ -0,0 +1,17 @@
+interface DTOExportInterface {
+    _id: string
+    name: string
+    email: string
+    created_at: string
+    skills: string[]
+    cpf: string
+    rg: string
+}
+
+class XlsGeneratorService {
+    async run() {
+        return
+    }
+}
+
+export { XlsGeneratorService }
