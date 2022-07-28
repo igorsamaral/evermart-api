9f0416b0854695b6f6ad3d7a54ae979bcf530f56
Igor Santana <igr.amaral@gmail.com>
2022-07-27 22:03:59 -0300

    refactor: remove o constructor e faz a chamada do service dentro do método

--- src/Presenters/CustomerPresenter.ts
+++ src/Presenters/CustomerPresenter.ts
@@ -1,23 +1,26 @@
 import { Request, Response } from 'express'
-import { CustomerService } from '../Services/Customer/CustomerService'
+import CustomerService from '../Services/Customer/CustomerService';
 import { XlsGeneratorService } from '../Services/Export/XlsGeneratorService'
 
 class CustomerPresenter {
-    private customerService: CustomerService
-
-    constructor() {
-        this.customerService = new CustomerService()
-    }
-
     async index(request: Request, response: Response) {
-        return response.json({})
+        try {
+            const customerService = new CustomerService()
+            const customers = await customerService.getAll()
+
+            return response.json(customers)
+        } catch (error) {
+            console.error(error)
+        }
     }
 
     async store(request: Request, response: Response) {
         const data = request.body
 
         try {
-            const customer = await this.customerService.create(data)
+            const customerService = new CustomerService()
+            const customer = await customerService.create(data)
+
             return response.json(customer)
         } catch (error) {
             console.error(error)
