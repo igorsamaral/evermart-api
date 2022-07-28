50bd6368a72acf32e2f2f018c39ff22f2dea2e82
Igor Santana <igr.amaral@gmail.com>
2022-07-27 22:14:21 -0300

    feat: add created_at no schema

--- src/Entities/Customer.ts
+++ src/Entities/Customer.ts
@@ -7,6 +7,7 @@ const CustomerSchema: Schema = new Schema({
     skills: { type: [String], required: true },
     cpf: { type: String, required: true },
     rg: { type: String, required: true },
+    created_at: { type: String, required: true },
 });
 
 export default mongoose.model<CustomerInterface>('Customer', CustomerSchema);
