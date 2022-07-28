fe40db4607fd728b818449716a82083cd34b2cad
Igor Santana <igr.amaral@gmail.com>
2022-07-27 23:27:55 -0300

    feat: implementa o serviço para gerar XLS com uso da lib excel4node

--- src/Services/Export/XlsGeneratorService.ts
+++ src/Services/Export/XlsGeneratorService.ts
@@ -1,17 +1,29 @@
-interface DTOExportInterface {
-    _id: string
-    name: string
-    email: string
-    created_at: string
-    skills: string[]
-    cpf: string
-    rg: string
-}
+import xl from 'excel4node'
 
-class XlsGeneratorService {
-    async run() {
-        return
+export default class XlsGeneratorService {
+    run(worksheetName: string, columns: string[], rows: object[]) {
+        const timestamp = new Date().getTime();
+
+        const wb = new xl.Workbook()
+        const ws = wb.addWorksheet(worksheetName)
+
+        let headingColumnIndex = 1
+        let rowIndex = 2
+
+        columns.forEach(heading => {
+            ws.cell(1, headingColumnIndex++).string(heading)
+        });
+
+        rows.forEach(record => {
+            let columnIndex = 1
+
+            Object.keys(record['_doc']).forEach(columnName => {
+                ws.cell(rowIndex, columnIndex++).string(JSON.stringify(record[columnName]))
+            })
+
+            rowIndex++
+        });
+
+        wb.write(timestamp + ".xlsx")
     }
 }
-
-export { XlsGeneratorService }
