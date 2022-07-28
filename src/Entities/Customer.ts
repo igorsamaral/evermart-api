2a69fcce9ccdb3bba0ba2b30af82a6b4f94cb69b
Igor Santana <igr.amaral@gmail.com>
2022-07-27 23:27:02 -0300

    feat: add getters para retornar data tratada

--- src/Entities/Customer.ts
+++ src/Entities/Customer.ts
@@ -1,13 +1,21 @@
 import mongoose, { Schema } from 'mongoose';
 import CustomerInterface from './types/CustomerInterface';
 
+const timeSince = (date: Date) => {
+    return date.toLocaleString()
+}
+
 const CustomerSchema: Schema = new Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
     skills: { type: [String], required: true },
     cpf: { type: String, required: true },
     rg: { type: String, required: true },
-    created_at: { type: String, required: true },
+    created_at: { type: Date, required: true, get: (date: Date) => timeSince(date) },
+}, {
+    timestamps: true,
+    toJSON: { getters: true, virtuals: true },
+    versionKey: false
 });
 
 export default mongoose.model<CustomerInterface>('Customer', CustomerSchema);
