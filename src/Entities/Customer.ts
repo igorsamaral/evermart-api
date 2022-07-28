b8e1f87dbb277b1c5f8fe41e7555e258f3cd2988
Igor Santana <igr.amaral@gmail.com>
2022-07-27 22:02:02 -0300

    refactor: otimiza o schema

--- src/Entities/Customer.ts
+++ src/Entities/Customer.ts
@@ -1,60 +1,12 @@
-import { Schema, model, connect } from 'mongoose';
-import { CustomerDTO } from './types/CustomerDTO';
+import mongoose, { Schema } from 'mongoose';
+import CustomerInterface from './types/CustomerInterface';
 
-/** Validators */
-function isEmailValid(email: string) {
-    const regexFilter = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
-    return regexFilter.test(email);
-}
-
-const customerSchema = new Schema<CustomerDTO>({
-    name: {
-        type: String,
-        required: [true, 'Invalid entries. Try again.'],
-    },
-    email: {
-        type: String,
-        required: [true, 'Invalid entries. Try again.'],
-        validate: [isEmailValid, 'Invalid entries. Try again.'],
-        unique: [true, 'Email already registered'],
-    },
-    created_at: {
-        type: String,
-        required: [true, 'Invalid entries. Try again.'],
-    },
-    skills: {
-        type: Array,
-        required: [true, 'Invalid entries. Try again.'],
-    },
-    cpf: {
-        type: String,
-        required: [true, 'Invalid entries. Try again.'],
-    },
-    rg: {
-        type: String,
-        required: [true, 'Invalid entries. Try again.'],
-    },
+const CustomerSchema: Schema = new Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    skills: { type: [String], required: true },
+    cpf: { type: String, required: true },
+    rg: { type: String, required: true },
 });
 
-const Customer = model<CustomerDTO>('Customer', customerSchema);
-
-// run().catch(err => console.log(err));
-
-// async function run() {
-//     await connect('mongodb+srv://dbTest:@Mayk2022@cluster0.zrv8j.mongodb.net/test');
-
-//     const customer = new Customer({
-//         name: 'Igor',
-//         email: 'igr.amaral@gmail.com',
-//         created_at: new Date(),
-//         skills: ['backEnd', 'aws cloud'],
-//         cpf: '111.222.333-44',
-//         rg: '111222333'
-//     });
-
-//     await customer.save();
-
-//     console.log(customer.email);
-// }
-
-export { Customer }
+export default mongoose.model<CustomerInterface>('Customer', CustomerSchema);
